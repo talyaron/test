f1837e0e599a61c4797683fbe750866ced262151
Tal Yaron <tal.yaron@gmail.com>
2019-02-17 09:09:11 +0200

    to public

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/get', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 let port = process.env.PORT || 3000;
 
